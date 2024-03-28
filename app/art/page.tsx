a7a656ca6cd385fe39eeed202ea31a99e1b8e473
Jonathan Silva <jonathan.silva167@gmail.com>
2024-03-28 16:05:12 -0400

    Add art and tattoo pages

---
+++ app/art/page.tsx
@@ -0,0 +1,9 @@
+const ArtPage = () => {
+	return (
+		<div>
+			<h1>Art Page</h1>
+		</div>
+	)
+}
+
+export default ArtPage
---
+++ app/tattoo/page.tsx
@@ -0,0 +1,9 @@
+const TattooPage = () => {
+	return (
+		<div>
+			<h1>Tattoo Page</h1>
+		</div>
+	)
+}
+
+export default TattooPage
