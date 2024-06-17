ff08232c42f8b4eeb5d7af8cc2db90810cba22e8
Jonathan Silva <jonathan.silva167@gmail.com>
2024-06-16 23:34:44 -0400

    test

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ import tattooLogo from "../assets/logos/tattoo_logo.png";
 import Image from "next/image";
 const artBg1 = "url(images/art/painting_1/FB_IMG_1712266614230.jpg)"
 const tattooBg1 = "url(images/tattoo/FB_IMG_1712266126903.jpg)"
+const artBg1 = "url(/images/art/painting_1/FB_IMG_1712266614230.jpg)"
+const tattooBg1 = "url(/images/tattoo/FB_IMG_1712266126903.jpg)"
 
 export default function Home() {
 	const [darkMode, setDarkMode] = useState(true);
